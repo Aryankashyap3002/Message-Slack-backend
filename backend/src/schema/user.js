260740dfc896ce5eb6987d7fa6304a530c55dcb3
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-03-11 11:55:09 +0530

    Added signup with zod validations

--- backend/src/schema/user.js
+++ backend/src/schema/user.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
  
  const userSchema = new mongoose.Schema(
@@ -20,6 +21,7 @@ import mongoose from 'mongoose';
        type: String,
        required: [true, 'Username is required'],
        unique: [true, 'Username already exists'],
+       minLength: [3, 'Username must be at least 3 characters'],
        match: [
          /^[a-zA-Z0-9]+$/,
          'Username must contain only letters and numbers'
@@ -33,9 +35,12 @@ import mongoose from 'mongoose';
  );
  
  userSchema.pre('save', function saveUser(next) {
-   const user = this;
-   user.avatar = `https://robohash.org/${user.username}`;
-   next();
+    const user = this;
+    const SALT = bcrypt.genSaltSync(9);
+    const hashedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = hashedPassword;
+    user.avatar = `https://robohash.org/${user.username}`;
+    next();
  });
  
  const User = mongoose.model('User', userSchema);
